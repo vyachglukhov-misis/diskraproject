58ffc6e0cabb89bc21499e1d7d2d863c801f7178
fridere <eduard.pochuev@mail.ru>
2024-05-26 06:28:09 +0300

    исправил EdgeSheavesList.py,подобно моему мру нахуй выпилил арки

--- Arc.py
+++
@@ -1,7 +0,0 @@
-class Arc:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-
-    def __lt__(self, other):
-        return self.start < other.start
--- EdgeSheavesList.py
+++ EdgeSheavesList.py
@@ -1,4 +1,3 @@
-from OrderedEdgeList import OrderedEdgeList
 from AdjacentList import AdjacentList
 from Edge import Edge
 
@@ -7,55 +6,86 @@ class EdgeSheavesList:
     def __init__(self, n, m):
         self.n = n
         self.m = m
-        self.starts = [-1 for _ in range(m)]
-        self.ends = [-1 for _ in range(m)]
-        self.sheave_heads = [-1 for _ in range(n)]
-        self.next_edge = [-1 for _ in range(m)]
-        self.oel = None
+        self.starts = []
+        self.ends = []
+        self.sheaf_heads = [-1 for _ in range(self.n)]
+        self.next_edge = [-1 for _ in range(self.m)]
+
+    def add(self, edge: Edge):
+        self.starts.append(edge.start)
+        self.ends.append(edge.end)
+
+        #когда закончили добавлять ребра, надо обновить значения в sheaf_heades и next_edge
+        if len(self.starts) == self.m:
+            for i in range(self.m):
+                v = self.starts[i]
+                self.next_edge[i] = self.sheaf_heads[v]
+                self.sheaf_heads[v] = i
 
     @staticmethod
-    def from_adjacent_list(adjacent_list: AdjacentList):
-        oel = OrderedEdgeList.from_adjacent_list(adjacent_list)
-        esl = EdgeSheavesList(adjacent_list.n, adjacent_list.m)
-        edge_index = 0
-        sheave_index = -1
-        last_source = -1
-        for i in range(oel.m):
-            if last_source != oel.starts[i]:
-                last_source = oel.starts[i]
-                sheave_index += 1
-                esl.sheave_heads[oel.starts[i]] = i
-                continue
-            esl.next_edge[i - 1] = i
+    def from_adjacent_list(adj_list: AdjacentList):
+        edges_count = 0
+        for edges in adj_list.list:
+            edges_count += len(edges)
 
-        for i, row in oel.list:
-            esl.sheave_heads[i] = edge_index
-            for j in range(1, len(row)):
-                 esl.next_edge[edge_index] = edge_index + 1
-                edge_index += 1
-        esl.oel = oel
+        esl = EdgeSheavesList(adj_list.n, edges_count)
+
+        for v_from, to_list in enumerate(adj_list.list):
+            for _, v_to in enumerate(to_list):
+                esl.add(Edge(v_from, v_to))
+
         return esl
 
     def print(self):
         for i in range(self.n):
             # просмотр пучка дуг, выходящих из вершины i
-            k = self.sheave_heads[i]
+            k = self.sheaf_heads[i]
             while k != -1:
                 print(self.starts[k], self.ends[k])
                 k = self.next_edge[k]
 
+        print("heads: ")
+        for sheaf_head in self.sheaf_heads:
+            print(sheaf_head)
+
+        print("next edge: ")
+        for edge_end in self.next_edge:
+            print(edge_end)
+
     def to_adjacent_list(self) -> AdjacentList:
-        adjacent_list = AdjacentList(self.n)
+        adj_list = AdjacentList(self.n)
 
         for i in range(self.n):
             start = self.starts[i]
             end = self.ends[i]
 
-            adjacent_list.add(Edge(start, end))
+            adj_list.add(Edge(start, end))
 
-        return adjacent_list
+        return adj_list
 
 
-arc = EdgeSheavesList(4, 5)
+# test
+#пример графа и ожидаемого результата на стр 24 учебника
+print("From adj list")
+adj_list = AdjacentList(4)
 
-arc.print()
+adj_list.add(Edge(0, 1))
+adj_list.add(Edge(1, 2))
+adj_list.add(Edge(2, 3))
+adj_list.add(Edge(1, 3))
+adj_list.add(Edge(0, 2))
+
+EdgeSheavesList.from_adjacent_list(adj_list).print()
+
+print("From constructor")
+esl = EdgeSheavesList(4, 5)
+
+esl.add(Edge(0, 1))
+esl.add(Edge(1, 2))
+esl.add(Edge(2, 3))
+esl.add(Edge(1, 3))
+esl.add(Edge(0, 2))
+
+esl.print()
+
+#всё вроде корректно - на бумажке проверил
