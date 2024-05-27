843dc70da685233ea5f8bb81c1e2a569342cb63f
achillescres <achillescres@gmail.com>
2024-05-27 21:27:44 +0300

    with normal ordered

--- OrderedEdgeList.py
+++ OrderedEdgeList.py
@@ -1,5 +1,5 @@
 from Edge import Edge
-from AdjacentList import AdjacentList
+from UnorderedEdgeList import UnorderedEdgeList
 
 '''
     Отсортированный список дуг
@@ -14,43 +14,21 @@ from AdjacentList import AdjacentList
 '''
 
 
-class OrderedEdgeList:
-    starts: list[int]
-    ends: list[int]
-    n: int
-    m: int
-
-    @staticmethod
-    def from_adjacent_list(adjacent_list: AdjacentList):
-        oel = OrderedEdgeList(adjacent_list.n)
-        for i, row in enumerate(adjacent_list.list):
-            for j in row:
-                oel.add(Edge(i, j))
-        return oel
-    
+class OrderedEdgeList(UnorderedEdgeList):
     def __init__(self, n):
-        self.n = n
-        self.starts = []
-        self.ends = []
-        self.m = 0
+        super().__init__(n)
 
     def add(self, edge: Edge):
-        assert edge.start < self.n
-        assert edge.end < self.n
-        self.starts.append(edge.start)
-        self.ends.append(edge.end)
+        # Еще можно бин поиском но сложность все равно останется линейной изза вставки
+        i = len(self.starts) - 1
+        while i >= 0 and Edge(self.starts[i], self.ends[i]) > edge:
+            i -= 1
+
+        self.starts.insert(i + 1, edge.start)
+        self.ends.insert(i + 1, edge.end)
         self.m += 1
 
-    def print(self):
-        for i in range(len(self.starts)):
-            print(f"Start: {self.starts[i]}, End: {self.ends[i]}")
 
-    def to_adjacent_list(self) -> AdjacentList:
-        adj_list = AdjacentList(self.n)
-        for i in range(len(self.starts)):
-            adj_list.add(Edge(self.starts[i], self.ends[i]))
-
-        return adj_list
 # # Test
 # s = OrderedEdgeList(10)
 # s.add(Edge(1, 2))
