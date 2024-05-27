c18b275ee3e2a86d3e67f498c1be9d192f7237d5
anylvl5600prodan <m2210359@edu.misis.ru>
2024-05-27 18:35:38 +0300

    not extends now 4 presentation

--- OrderedEdgeList.py
+++ OrderedEdgeList.py
@@ -1,10 +1,26 @@
 from Edge import Edge
 from UnorderedEdgeList import UnorderedEdgeList
+from AdjacentList import AdjacentList
 
 
-class OrderedEdgeList(UnorderedEdgeList):
+class OrderedEdgeList():
+    starts: list[int]
+    ends: list[int]
+    n: int
+    m: int
+
+    @staticmethod
+    def from_adjacent_list(adjacent_list: AdjacentList):
+        oel = OrderedEdgeList(adjacent_list.n)
+        for i, row in enumerate(adjacent_list.list):
+            for j in row:
+                oel.add(Edge(i, j))
+        return oel
+    
     def __init__(self, n):
-        super().__init__(n)
+        self.n = n
+        self.starts = []
+        self.ends = []
 
     def add(self, edge: Edge):
         # Еще можно бин поиском
@@ -16,9 +32,13 @@ class OrderedEdgeList(UnorderedEdgeList):
         self.ends.insert(i + 1, edge.end)
         self.m += 1
 
+    def print(self):
+        for i in range(len(self.starts)):
+            print(f"Start: {self.starts[i]}, End: {self.ends[i]}")
 
-# s = OrderedEdgeList(10)
-# s.add(Edge(1, 2))
-# s.add(Edge(2, 4))
-# s.add(Edge(0, 2))
-# s.print()
+    def to_adjacent_list(self) -> AdjacentList:
+        adj_list = AdjacentList(self.n)
+        for i in range(len(self.starts)):
+            adj_list.add(Edge(self.starts[i], self.ends[i]))
+
+        return adj_list
